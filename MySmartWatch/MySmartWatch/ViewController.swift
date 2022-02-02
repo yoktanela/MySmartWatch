d40cd9a67f144372fed324c32e855b92f283095f
yoktanela <elanur.yoktan@yemeksepeti.com>
2022-02-02 10:22:50 +0300

    Initial commit

---
+++ MySmartWatch/MySmartWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySmartWatch
+//
+//  Created by Elanur Yoktan on 2.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
