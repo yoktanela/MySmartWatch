bcd39d3cd0eb80e59731cd80988fbe8f346a14b9
yoktanela <elanur.yoktan@yemeksepeti.com>
2022-02-09 13:21:23 +0300

    Start periodic scan and add a button to control scanning

--- MySmartWatch/MySmartWatch/Controllers/ScanViewController.swift
+++ MySmartWatch/MySmartWatch/Controllers/ScanViewController.swift
@@ -16,21 +16,29 @@ class ScanViewController: UIViewController {
     var queue: DispatchQueue!
     private var centralManager: CBCentralManager!
     private var scannedPeripherals = BehaviorRelay<[HeartRatePeripheral]>(value: [])
+    private var disposeBag = DisposeBag()
+    var timerForScan = Timer()
     
     var peripheralTableView: UITableView = {
-        return UITableView()
+        var tableView = UITableView()
+        tableView.contentInset = UIEdgeInsets(top: 10.0, left: 0.0, bottom: 10.0, right: 0.0)
+        return tableView
+    }()
+    
+    var explanationLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = UIColor(named: "titleColor")
+        lbl.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        lbl.textAlignment = .center
+        lbl.numberOfLines = 0
+        return lbl
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.peripheralTableView = UITableView(frame: self.view.frame)
-        self.view.addSubview(peripheralTableView)
-        let topConstraint = self.peripheralTableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0)
-        self.view.addConstraints([topConstraint])
-        
-        peripheralTableView.register(PeripheralViewCell.self, forCellReuseIdentifier: "peripheralViewCell")
-        peripheralTableView.separatorStyle = .none
+        self.view.backgroundColor = UIColor.systemBackground
+        self.setSubviews()
         
         self.scannedPeripherals.asObservable()
             .bind(to: self.peripheralTableView.rx.items) { (tableView, row, element ) in
@@ -39,8 +47,67 @@ class ScanViewController: UIViewController {
                 return cell
             }
         
+        // Create CBCentralManager
         queue = DispatchQueue(label: "CentralManager")
         centralManager = CBCentralManager(delegate: self, queue: queue, options: nil)
+    }
+    
+    func setSubviews() {
+        self.view.addSubview(explanationLabel)
+        explanationLabel.translatesAutoresizingMaskIntoConstraints = false
+        let topLblConstariant = explanationLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10.0)
+        let leftLblConstraint = explanationLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20.0)
+        let rightLblConstraint = explanationLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -20.0)
+        self.view.addConstraints([topLblConstariant, leftLblConstraint, rightLblConstraint])
+        
+        peripheralTableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(peripheralTableView)
+        let topConstraint = self.peripheralTableView.topAnchor.constraint(equalTo: explanationLabel.bottomAnchor, constant: 10.0)
+        let bottomConstraint = self.peripheralTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 30)
+        let leftConstraint = self.peripheralTableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0.0)
+        let rightConstraint = self.peripheralTableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0.0)
+        self.view.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint])
+        
+        peripheralTableView.register(PeripheralViewCell.self, forCellReuseIdentifier: "peripheralViewCell")
+        peripheralTableView.separatorStyle = .none
+        
+        self.navigationItem.title = "Available Devices"
+        let button = UIBarButtonItem()
+        button.tintColor = UIColor(named: "buttonColor")
+        button.title = "Stop Scan"
+        self.navigationItem.setRightBarButtonItems([button], animated: false)
+        self.navigationItem.rightBarButtonItem?
+            .rx.tap.subscribe(onNext: { _ in
+                if (self.centralManager.isScanning) {
+                    self.centralManager.stopScan()
+                    self.timerForScan.invalidate()
+                    button.title = "Start Scan"
+                } else {
+                    self.startPeriodicScan()
+                    button.title = "Stop Scan"
+                }
+            }).disposed(by: disposeBag)
+        
+        scannedPeripherals.flatMap{ peripherals -> Observable<Bool> in
+            return .just(peripherals.count != 0)
+        }.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { hasElements in
+            if hasElements {
+                self.explanationLabel.text = "Select one of the smart watch to connect and monitor your heart rate, steps and sleep quality."
+            }else {
+                self.explanationLabel.text = ""
+            }
+            }).disposed(by: disposeBag)
+    }
+    
+    func startPeriodicScan() {
+        DispatchQueue.main.async {
+            self.timerForScan = Timer.scheduledTimer(withTimeInterval: TimeInterval(0.1), repeats: true) { _ in
+                self.centralManager.stopScan()
+                self.centralManager.scanForPeripherals(withServices: [CBUUID(string: Constants.heartRatePeripheralServiceUUID)])
+            }
+            self.timerForScan.fire()
+        }
     }
 }
 
@@ -59,7 +126,7 @@ extension ScanViewController: CBCentralManagerDelegate {
         case .poweredOff:
             print("poweredOff")
         case .poweredOn:
-            self.centralManager.scanForPeripherals(withServices: [CBUUID(string: Constants.heartRatePeripheralServiceUUID)])
+            self.startPeriodicScan()
         @unknown default:
             print("default")
         }
--- MySmartWatch/MySmartWatch/Helpers/Constants.swift
+++ MySmartWatch/MySmartWatch/Helpers/Constants.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 class Constants {
-    static let heartRatePeripheralServiceUUID = "7905FF00-B5CE-4E99-A40F-4B1E122D00D0"
+    static let heartRatePeripheralServiceUUID = "180D"
+    static let smartWatchPeripheralServiceUUID = "7905FF00-B5CE-4E99-A40F-4B1E122D00D0"
 }
